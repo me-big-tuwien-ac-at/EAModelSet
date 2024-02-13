eb9a720afbedd6e61eb06e1b6982b2129ba82441
plglaser <plg741@gmail.com>
2024-02-13 13:47:37 +0100

    update mongodb start script

--- scripts/startMongoDB.sh
+++ scripts/startMongoDB.sh
@@ -1,1 +1,3 @@
-docker run --rm --name mongodb mongodb/mongodb-community-server
+#docker run --rm --name mongodb mongodb/mongodb-community-server
+docker run --rm --name mongo -p 27017:27017 -d mongodb/mongodb-community-server:latest
+
