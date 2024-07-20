f827ec76fed2ec23e326bd29f8ef463b28a7e7df
plglaser <plg741@gmail.com>
2024-07-20 22:30:22 +0200

    feat(python-cli): add version info and display help if no args provided

---
+++ python-cli/eamodelset/__init__.py
@@ -0,0 +1,24 @@
+"""
+EA ModelSet CLI
+"""
+
+__app_name__ = "eamodelset"
+__version__ = "0.1.0"
+
+(
+    SUCCESS, 
+    FILE_ERROR, 
+    DIR_ERROR,
+    DATA_READ_ERROR,
+    DATA_WRITE_ERROR,
+    JSON_ERROR,
+    ID_ERROR
+) = range(7)
+
+ERRORS = {
+    FILE_ERROR: "config file error",
+    DIR_ERROR: "config directory error",
+    DATA_READ_ERROR: "dataset read error",
+    DATA_WRITE_ERROR: "dataset write error",
+    ID_ERROR: "model id error",
+}
--- python-cli/eamodelset/main.py
+++ python-cli/eamodelset/main.py
@@ -1,22 +1,27 @@
 import typer
-from enum import Enum
-from rich import print
-from pathlib import Path
-from typing_extensions import Annotated
+from typing import Optional
 
+from eamodelset import __app_name__, __version__
 from eamodelset.commands.transform import transform
 
-app = typer.Typer()
+app = typer.Typer(no_args_is_help=True)
 app.command(name="transform")(transform)
 
-# add
-# remove
-# validate
-# extract
-# archive
+def _version_callback(value: bool) -> None:
+    if value:
+        typer.echo(f"{__app_name__} v{__version__}")
+        raise typer.Exit()
+
 
 @app.callback()
-def callback():
-    """
-    EA ModelSet CLI
-    """
+def main(
+    version: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        "-v",
+        help="Show the application's version and exit.",
+        callback=_version_callback,
+        is_eager=True,
+    )
+) -> None:
+    return
